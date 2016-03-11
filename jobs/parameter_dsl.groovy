7aab82f0575fef866cf84a294ec35c3b5d8da655
Navdeep Singh <navdeep@Navdeeps-MacBook-Pro.local>
2016-03-11 08:03:59 +0530

    Added new Job

---
+++ jobs/parameter_dsl.groovy
@@ -0,0 +1,20 @@
+job('chef-cookbooks-s3-dsl-job') {
+
+readFileFromWorkspace('parameters.xml')
+
+  scm {
+        git {
+            remote {
+                github('nclouds/vpn', 'https','bitbucket.org')
+                credentials('navdeep-bitbucket-account')
+            }
+           branch('*/$deploy_branch')
+        }
+    }
+
+  steps {
+    shell('berks package cookbooks.tar.gz')
+    shell('aws s3 cp cookbooks.tar.gz s3://$s3_bucket_name/')
+  }
+
+}
