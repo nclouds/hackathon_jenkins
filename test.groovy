fbbaf2bf43ce1cee6ce4128641c7e360415aead1
Artem Kobrin <kobrin.artem@gmail.com>
2016-03-11 05:32:10 +0200

    demo

--- test.groovy
+++ test.groovy
@@ -23,3 +23,25 @@ job("${params.conf.env}-ken-demo-dsl-job") {
         shell('build.sh')
   }
 }
+
+job("${params.conf.env}-ken-demo-dsl-job") {
+  parameters {
+    stringParam('deploy_branch', "${params.config.branch}")
+     stringParam('env', "${params.conf.env}")
+   }
+
+  scm {
+        git {
+            remote {
+                github('nclouds/ken', 'https','bitbucket.org')
+                credentials('navdeep-bitbucket-account')
+            }
+           branch('*/$deploy_branch')
+        }
+    }
+
+  steps {
+    shell('test')
+  }
+
+}
